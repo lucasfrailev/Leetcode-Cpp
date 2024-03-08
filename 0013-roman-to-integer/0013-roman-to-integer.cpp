2d1d98abbad781b9bf19d892790b7a8802109d15
Lucas Fraile <47170229+lucasfrailev@users.noreply.github.com>
2024-03-07 19:02:49 -0800

    Time: 7 ms (75.71%), Space: 12.8 MB (20.34%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        std::unordered_map<string,int> dict    {
-        {"I", 1},
-        {"V", 5},
-        {"X", 10},
-        {"L", 50},
-        {"C", 100},
-        {"D", 500},
-        {"M", 1000},
+        std::unordered_map<char,int> dict    {
+        {'I', 1},
+        {'V', 5},
+        {'X', 10},
+        {'L', 50},
+        {'C', 100},
+        {'D', 500},
+        {'M', 1000},
     };
         int previous = 1000;
         int total = 0;
         for (std::string::size_type i = 0; i <s.size(); ++i){
-            int current = dict[s.substr(i, 1)];
+            int current = dict[s[i]];
             if (previous < current){
                 total = total - 2 * previous + current;
             } else{
