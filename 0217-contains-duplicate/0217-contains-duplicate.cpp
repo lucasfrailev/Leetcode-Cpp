0c0009a7dc90f9e85a3589cf0bf45786e0d18f8a
Lucas Fraile <47170229+lucasfrailev@users.noreply.github.com>
2024-03-11 22:50:29 -0700

    Time: 110 ms (27.53%), Space: 73.1 MB (55.86%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
         unordered_map<int,bool> uniques;
+        sort(nums.begin(), nums.end()); 
         for(int i=0;i<nums.size();++i){
             if (uniques.contains(nums[i])){
                 return true;
