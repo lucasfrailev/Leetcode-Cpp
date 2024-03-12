9ae2ba910b073e735703c49aca3ea124d3e24364
Lucas Fraile <47170229+lucasfrailev@users.noreply.github.com>
2024-03-11 23:01:13 -0700

    Time: 75 ms (96.37%), Space: 73.1 MB (40.81%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
         unordered_set<int> uniques;
         for (int num : nums){
             if (uniques.contains(num)){
