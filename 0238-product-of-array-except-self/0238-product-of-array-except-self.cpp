f0673ad6483545978197dbf5e325f9c1d1a610d5
Lucas Fraile <47170229+lucasfrailev@users.noreply.github.com>
2024-03-12 11:28:26 -0700

    Time: 11 ms (90.53%), Space: 27.5 MB (22.67%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -4,21 +4,17 @@ public:
         ios_base::sync_with_stdio(false);
         cin.tie(NULL);
         cout.tie(NULL);
-        unordered_map<int,int> unique;
-        vector<int> answer(nums.size()), auxs = nums;
-        for (int index = 0; index <nums.size(); index++){
-            if (unique.contains(auxs[index])){
-               answer[index]=unique[auxs[index]]; 
-            } else {
-            auxs[index] = 1;
-            int product = 1;
-            for (int aux:auxs){
-                product *= aux; 
-            }
-            answer[index]=product;
-            auxs = nums;
-            unique[auxs[index]]=product;
-            }
+        int n = nums.size();
+        vector<int> answer;
+        answer.push_back(1);
+        for (int i = 1; i<=n-1;i++){
+            answer.push_back(nums[i-1]*answer[i-1]);
+        }
+        printf("\n");
+        int aux_prod = 1;
+        for (int i=n-1;i>=0;--i){
+            answer[i] *= aux_prod;
+            aux_prod *= nums[i];
         }
         return answer;
     }
