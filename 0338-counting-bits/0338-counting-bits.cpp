20594ed90a8692f960995979cd957a605ec6edba
Lucas Fraile <47170229+lucasfrailev@users.noreply.github.com>
2024-03-15 20:53:34 -0700

    Time: 0 ms (100.00%), Space: 9 MB (52.91%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -6,7 +6,8 @@ public:
         cout.tie(NULL);
         vector<int> bits(n+1);
         for (int i = 0; i<n+1;i++){
-            bits[i] = bits[i/2]+i%2; 
+            bits[i] = bits[i/2]+i%2;
+
         }
         return bits;        
     }
