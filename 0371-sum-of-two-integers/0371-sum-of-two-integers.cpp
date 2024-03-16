f392508e2474015efcb74257fe8a696220b28696
Lucas Fraile <47170229+lucasfrailev@users.noreply.github.com>
2024-03-15 20:30:12 -0700

    Time: 0 ms (100.00%), Space: 7.2 MB (21.38%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int getSum(int a, int b) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
         int aux = (a ^ b), carry = (a & b) << 1, mem;
         while (true){
             if (!(carry & aux)){
