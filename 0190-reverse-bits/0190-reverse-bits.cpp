7752abd853bea949ceb204f23d2b2b1758693340
Lucas Fraile <47170229+lucasfrailev@users.noreply.github.com>
2024-03-15 21:15:25 -0700

    Time: 0 ms (100.00%), Space: 7.3 MB (6.75%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.cpp
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     uint32_t reverseBits(uint32_t n) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
         uint32_t flipped = 0;
         for (int i = 0;i<32;i++){
             flipped <<=1;
