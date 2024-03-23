363f5cd2dc5a8ee89daf76b8be11b4ac1cd75f6a
Lucas Fraile <47170229+lucasfrailev@users.noreply.github.com>
2024-03-23 01:05:07 -0700

    Time: 13 ms (78.47%), Space: 26 MB (6.90%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        vector<unordered_set<char>> hor(9,unordered_set<char>()),vert(9,unordered_set<char>()),square(9,unordered_set<char>());
+        for(int i = 0;i<9;i++){
+            for (int j = 0;j<9;j++){
+                if (board[i][j] != '.'){
+                    if (hor[i].contains(board[i][j])) return false;
+                    hor[i].insert(board[i][j]);
+                    if (vert[j].contains(board[i][j])) return false;
+                    vert[j].insert(board[i][j]);
+                    int square_index = 3*(i/3)+(j/3);
+                    if (square[square_index].contains(board[i][j])) return false;
+                    square[square_index].insert(board[i][j]);
+                }
+            }
+        }
+        return true;
+    }
+};
