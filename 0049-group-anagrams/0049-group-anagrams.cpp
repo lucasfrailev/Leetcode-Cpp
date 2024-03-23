f0a1cb0362b845d291bc28e2dddc600758a4d427
Lucas Fraile <47170229+lucasfrailev@users.noreply.github.com>
2024-03-22 19:39:24 -0700

    Time: 24 ms (76.00%), Space: 22.6 MB (89.47%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -4,46 +4,26 @@ public:
         ios_base::sync_with_stdio(false);
         cin.tie(NULL);
         cout.tie(NULL);
-        unordered_map<int,vector<int>> lengths;
+        int n = strs.size();
+        if (n<2) return {strs};
         vector<vector<string>> anagrams;
-        for(int i=0;i<strs.size();i++){
-            bool aux_flag= false;
-            if (lengths.contains(strs[i].size())){
-                for(int j:lengths[strs[i].size()]){
-                    if (isAnagram(strs[i],anagrams[j][0])){
-                        anagrams[j].push_back({strs[i]});
-                        aux_flag = true;
-                        break;
-                    }
+        vector<pair<string,int>> aux_strs;
+        for(int i=0;i<n;i++){
+            string aux = strs[i];
+            sort(aux.begin(),aux.end());
+            aux_strs.push_back({aux,i});
+        }
+        sort(aux_strs.begin(),aux_strs.end());
+        int i=0;
+        anagrams.push_back({strs[aux_strs[0].second]});
+        for(int j=1;j<n;j++){
+                if (aux_strs[j].first == aux_strs[j-1].first){
+                    anagrams[i].push_back({strs[aux_strs[j].second]});
+                } else {
+                    i++;
+                    anagrams.push_back({strs[aux_strs[j].second]});
                 }
-                if (aux_flag == false){
-                    anagrams.push_back({strs[i]});
-                    lengths[strs[i].size()].push_back(anagrams.size()-1);
-                }
-            } else {
-               anagrams.push_back({strs[i]});
-               lengths[strs[i].size()] = {int(anagrams.size()-1)}; 
             }
-        }
         return anagrams;
-    }
-    
-    bool isAnagram(string s, string t) {
-        ios_base::sync_with_stdio(false);
-        cin.tie(NULL);
-        cout.tie(NULL);
-        if (s.size()!=t.size()) return false;
-        vector<int> count(26,0);
-        for (char letter:s){
-            count[letter-'a']++;
-        }
-        for (char letter:t){
-            count[letter-'a']--;
-            if (count[letter-'a']<0) return false;
-        }
-        for (int i = 0; i<26;i++){
-            if (count[i]!=0) return false;
-        }
-        return true;
     }
 };
