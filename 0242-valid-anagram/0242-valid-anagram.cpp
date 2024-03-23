66dedaa96c6cb591cb231ff1e98aa75b71857e16
Lucas Fraile <47170229+lucasfrailev@users.noreply.github.com>
2024-03-22 17:39:11 -0700

    Time: 9 ms (50.56%), Space: 8.5 MB (59.66%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        int n = s.size();
-        if (n != t.size()) return false;
+        if (s.size() != t.size()) return false;
         sort(s.begin(), s.end());
         sort(t.begin(), t.end());
-        for (int i = 0; i<n; i++) {
+        for (int i = 0; i<s.size(); i++) {
             if (s[i]!=t[i]) return false;
         }
         return true;   
