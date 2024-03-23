922570069564576f12561e4df0d730e14e93bdcd
Lucas Fraile <47170229+lucasfrailev@users.noreply.github.com>
2024-03-22 17:39:53 -0700

    Time: 11 ms (36.95%), Space: 8.6 MB (59.66%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -4,10 +4,11 @@ public:
         ios_base::sync_with_stdio(false);
         cin.tie(NULL);
         cout.tie(NULL);
-        if (s.size() != t.size()) return false;
+        int n = s.size();
+        if (n != t.size()) return false;
         sort(s.begin(), s.end());
         sort(t.begin(), t.end());
-        for (int i = 0; i<s.size(); i++) {
+        for (int i = 0; i<n; i++) {
             if (s[i]!=t[i]) return false;
         }
         return true;   
