8cc2a4c883df171274cda02d6bf056a12c310e91
Lucas Fraile <47170229+lucasfrailev@users.noreply.github.com>
2024-03-22 17:39:35 -0700

    Time: 10 ms (44.72%), Space: 8.6 MB (59.66%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
         if (s.size() != t.size()) return false;
         sort(s.begin(), s.end());
         sort(t.begin(), t.end());
