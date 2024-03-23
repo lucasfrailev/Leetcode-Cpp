0450a9e98fb78f57dbb97297243d0a0245c0650a
Lucas Fraile <47170229+lucasfrailev@users.noreply.github.com>
2024-03-22 17:40:31 -0700

    Time: 7 ms (77.06%), Space: 8.4 MB (82.84%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -4,13 +4,8 @@ public:
         ios_base::sync_with_stdio(false);
         cin.tie(NULL);
         cout.tie(NULL);
-        int n = s.size();
-        if (n != t.size()) return false;
         sort(s.begin(), s.end());
         sort(t.begin(), t.end());
-        for (int i = 0; i<n; i++) {
-            if (s[i]!=t[i]) return false;
-        }
-        return true;   
+        return s==t;   
     }
 };
