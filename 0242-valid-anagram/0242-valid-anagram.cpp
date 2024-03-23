ba5222cd3245dffc5f3d414aaa1cbd1e57a2b441
Lucas Fraile <47170229+lucasfrailev@users.noreply.github.com>
2024-03-22 17:38:28 -0700

    Time: 13 ms (25.77%), Space: 8.4 MB (96.75%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,27 +1,13 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        unordered_map<char,int> letters;
-        for (char& i : s){
-            if (letters.contains(i)){
-                letters[i]=letters[i]+1;
-            } else {
-                letters[i]=1;
-            }
+        int n = s.size();
+        if (n != t.size()) return false;
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        for (int i = 0; i<n; i++) {
+            if (s[i]!=t[i]) return false;
         }
-        for (char& i : t){
-            if (!letters.contains(i)){
-                return false;
-            }
-            letters[i]=letters[i]-1;
-            if (letters[i]==0){
-                letters.erase(i);
-            }
-            
-        }
-        if (letters.empty()){
-            return true;
-        }
-        return false;
+        return true;   
     }
 };
