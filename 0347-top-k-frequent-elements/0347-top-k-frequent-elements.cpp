2cb0c5c974817042343202e6a76aa37f93e3bbf6
Lucas Fraile <47170229+lucasfrailev@users.noreply.github.com>
2024-03-22 20:31:01 -0700

    Time: 13 ms (43.75%), Space: 17.4 MB (23.54%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -4,48 +4,18 @@ public:
         ios_base::sync_with_stdio(false);
         cin.tie(NULL);
         cout.tie(NULL);
-        if (nums.size()==k) return nums;
-        sort(nums.begin(),nums.end());
-        vector<int> ans(k,0), frequencies(k,0);
-        int count = 1, index = 0;
-        sort(nums.begin(),nums.end());
-        for (int i=1;i<nums.size();i++){
-            if (nums[i]==nums[i-1]){
-                count++;
-            } else {
-                bool flag = false;
-                for (int j=k-1;j>=0;j--){
-                    if (count >= frequencies[j]){
-                        flag = true;
-                    } else {
-                        index = j+1;
-                        break;
-                    }
-                }
-                if (flag){
-                    frequencies.insert(frequencies.begin()+index,1,count);
-                    ans.insert(ans.begin()+index,1,nums[i-1]);
-                    frequencies.pop_back();
-                    ans.pop_back();
-                }
-                count = 1;
-                index = 0;
-            }
+        unordered_map<int,int> freqs;
+        vector<pair<int,int>> aux_sortable;
+        vector<int> ans;
+        for (int num:nums){
+            freqs[num]++;
         }
-        bool flag = false;
-        for (int j=k-1;j>=0;j--){
-            if (count >= frequencies[j]){
-                flag = true;
-            } else {
-                index = j+1;
-                break;
-            }
+        for (pair<int,int> i:freqs){
+            aux_sortable.push_back({i.second,i.first});
         }
-        if (flag){
-            frequencies.insert(frequencies.begin()+index,1,count);
-            ans.insert(ans.begin()+index,1,nums[nums.size()-1]);
-            frequencies.pop_back();
-            ans.pop_back();
+        sort(aux_sortable.begin(),aux_sortable.end());
+        for (int j = 0;j<k;j++){
+            ans.push_back(aux_sortable[aux_sortable.size()-j-1].second);
         }
         return ans;
     }
