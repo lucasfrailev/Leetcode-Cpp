ceeb358f0c978b6fdd8d7b890a209b7069dc4e44
Lucas Fraile <47170229+lucasfrailev@users.noreply.github.com>
2024-03-22 20:50:01 -0700

    Time: 18 ms (38.85%), Space: 26.4 MB (60.60%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -5,17 +5,15 @@ public:
         cin.tie(NULL);
         cout.tie(NULL);
         int n = nums.size();
-        vector<int> answer;
-        answer.push_back(1);
-        for (int i = 1; i<=n-1;i++){
-            answer.push_back(nums[i-1]*answer[i-1]);
+        vector<int> ans(n,1);
+        for (int i = 1; i < n;i++){
+            ans[i] = ans[i-1] * nums[i-1]; 
         }
-        printf("\n");
-        int aux_prod = 1;
-        for (int i=n-1;i>=0;--i){
-            answer[i] *= aux_prod;
-            aux_prod *= nums[i];
+        int right = 1;
+        for (int i = n-1; i >= 0 ;i--){
+            ans[i] *= right;
+            right *= nums[i]; 
         }
-        return answer;
+        return ans;
     }
 };
