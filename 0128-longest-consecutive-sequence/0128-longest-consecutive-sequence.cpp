08e844317441cb2f17894ec50245dd565adb780a
Lucas Fraile <47170229+lucasfrailev@users.noreply.github.com>
2024-03-24 20:18:22 -0700

    Time: 820 ms (22.78%), Space: 74 MB (47.08%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -8,8 +8,8 @@ public:
         int max_length = 0, length = 1;
         for (int num:nums){
             if (set.find(num-1)==set.end()){
-                length = 0;
-                while (set.contains(num+length)){
+                length = 1;
+                while (set.find(num+length)!=set.end()){
                     length++;
                 }
                 if (length>max_length) max_length = length;
