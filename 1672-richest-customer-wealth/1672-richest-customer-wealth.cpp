b0985bb5ab93eebb78c088cbb14f4273b5799079
Lucas Fraile <47170229+lucasfrailev@users.noreply.github.com>
2024-04-06 13:28:16 -0700

    Time: 4 ms (35.42%), Space: 10.2 MB (32.65%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.cpp
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
     int maximumWealth(vector<vector<int>>& accounts) {
-        vector<int> wealth(accounts.size(),0);
         int max_wealth = 0;
         for (int i =0; i<accounts.size();i++){
-            for (int j = 0; j< accounts[0].size();j++)
-            wealth[i] += accounts[i][j];
-            max_wealth = (wealth[i]>max_wealth ? wealth[i] : max_wealth);
+            int wealth = 0;
+            for (int j = 0; j< accounts[0].size();j++){
+            wealth += accounts[i][j];
+            }
+            max_wealth = (wealth>max_wealth ? wealth : max_wealth);
         }
         return max_wealth;
     }
