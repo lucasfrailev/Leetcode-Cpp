69fa2f75e51e5656694cb440ec1d65023b17be91
Lucas Fraile <47170229+lucasfrailev@users.noreply.github.com>
2024-04-06 17:19:48 -0700

    Time: 177 ms (94.22%), Space: 304.9 MB (5.20%) - LeetHub

--- 2260-minimum-consecutive-cards-to-pick-up/2260-minimum-consecutive-cards-to-pick-up.cpp
+++ 2260-minimum-consecutive-cards-to-pick-up/2260-minimum-consecutive-cards-to-pick-up.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     int minimumCardPickup(vector<int>& cards) {
-        unordered_map<int,int> pairs;
+        vector<int> pairs(1000001,-1);
         int min_dist = INT_MAX;
         for(int index = 0;index<cards.size();index++){
-                if (pairs.contains(cards[index])){
+                if (pairs[cards[index]]>=0){
                     min_dist = min(min_dist, -pairs[cards[index]]+index+1);
                 } 
             pairs[cards[index]]=index;
