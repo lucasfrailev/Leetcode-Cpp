15bfc73f5f39d7ee6c39f19c358e43f286434698
Lucas Fraile <47170229+lucasfrailev@users.noreply.github.com>
2024-05-01 17:38:18 -0700

    Time: 4 ms (13.89%), Space: 7.8 MB (23.00%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -6,18 +6,19 @@ public:
         close['{'] = '}';
         close['['] = ']';
         close['0'] = '0';
-        vector<char> mem = {'0'};
+        stack<char> mem; 
+        mem.push('0');
         const char *str = "({[";
         for (char c : s){
             if (strchr(str,c)!=nullptr){
-                mem.push_back(c);
-            } else if(c == close[mem.back()]){
-                mem.pop_back();
+                mem.push(c);
+            } else if(c == close[mem.top()]){
+                mem.pop();
             } else {
                 return false;
             }
         }
-        if (mem.back() == '0'){
+        if (mem.top() == '0'){
             return true;
         }
         return false;
