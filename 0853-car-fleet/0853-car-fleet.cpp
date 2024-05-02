fbece95372e6de284bbd1172f3d9289f4d4669ff
Lucas Fraile <47170229+lucasfrailev@users.noreply.github.com>
2024-05-02 00:25:06 -0700

    Time: 126 ms (77.92%), Space: 89.3 MB (34.06%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.cpp
+++ 0853-car-fleet/0853-car-fleet.cpp
@@ -6,7 +6,9 @@ public:
         for(int i = 0;i<n;i++){
             aux.push_back({position[i],double(target-position[i])/double(speed[i])});
         }
-        sort(aux.begin(),aux.end());
+        sort(aux.begin(), aux.end(), [](const pair<int, int>& a, const pair<int, int>& b) {
+            return a.first < b.first;
+        });
         double last_max_time = DBL_MIN;
         int ans = 0;
         for (int i = n-1;i>=0;i--){
