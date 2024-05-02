a9df21ba5379e0e984ecc17794a832c06e38cd97
Lucas Fraile <47170229+lucasfrailev@users.noreply.github.com>
2024-05-02 00:30:14 -0700

    Time: 89 ms (98.35%), Space: 74.8 MB (99.21%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.cpp
+++ 0853-car-fleet/0853-car-fleet.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     int carFleet(int target, vector<int>& position, vector<int>& speed) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(0);
+        cout.tie(0);     
+
         int n =position.size();
         vector<int> aux(n,0);
         iota(aux.begin(),aux.end(),0);
