cb9e330d313b8327a841656951948a61d1b6dc54
Lucas Fraile <47170229+lucasfrailev@users.noreply.github.com>
2024-05-01 19:04:23 -0700

    Time: 3 ms (97.07%), Space: 15.1 MB (99.96%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -1,42 +1,38 @@
 class Solution {
 public:
     int evalRPN(vector<string>& tokens) {
-    while(tokens.size()>1){
+    vector<int> auxvector;
         int aux;
-        bool flag;
         for (int i = 0; i<tokens.size();i++){
             switch (tokens[i].back()){
                 case '+':
-                    aux = stoi(tokens[i-2]) + stoi(tokens[i-1]);
-                    tokens[i-2] = to_string(aux);
-                    tokens.erase(tokens.begin()+i-1,tokens.begin()+i+1);
-                    flag = true;
+                    aux = auxvector.rbegin()[1] + auxvector.rbegin()[0];
+                    auxvector.pop_back();
+                    auxvector.pop_back();
+                    auxvector.push_back(aux);
                     break;
                 case '-':
-                    aux = stoi(tokens[i-2]) - stoi(tokens[i-1]);
-                    tokens[i-2] = to_string(aux);
-                    tokens.erase(tokens.begin()+i-1,tokens.begin()+i+1);
-                    flag = true;
+                    aux = auxvector.rbegin()[1] - auxvector.rbegin()[0];
+                    auxvector.pop_back();
+                    auxvector.pop_back();
+                    auxvector.push_back(aux);
                     break;
                 case '/':
-                    aux = stoi(tokens[i-2]) / stoi(tokens[i-1]);
-                    tokens[i-2] = to_string(aux);
-                    tokens.erase(tokens.begin()+i-1,tokens.begin()+i+1);
-                    flag = true;
+                    aux = auxvector.rbegin()[1] / auxvector.rbegin()[0];
+                    auxvector.pop_back();
+                    auxvector.pop_back();
+                    auxvector.push_back(aux);
                     break;
                 case '*':
-                    aux = stoi(tokens[i-2]) * stoi(tokens[i-1]);
-                    tokens[i-2] = to_string(aux);
-                    tokens.erase(tokens.begin()+i-1,tokens.begin()+i+1);
-                    flag = true;
+                    aux = auxvector.rbegin()[1] * auxvector.rbegin()[0];
+                    auxvector.pop_back();
+                    auxvector.pop_back();
+                    auxvector.push_back(aux);
                     break;
+                default:
+                    auxvector.push_back(stoi(tokens[i]));
             }
-            if(flag){
-                flag = false; 
-                break;
-            }
-        }
     }
-    return stoi(tokens[0]);
+    return auxvector[0];
     }
 };
