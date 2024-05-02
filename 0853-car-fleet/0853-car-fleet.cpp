43953387347a4d6b3dccbd974b7a8a5b74b16ca8
Lucas Fraile <47170229+lucasfrailev@users.noreply.github.com>
2024-05-02 00:07:12 -0700

    Time: 176 ms (24.21%), Space: 97.3 MB (15.92%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.cpp
+++ 0853-car-fleet/0853-car-fleet.cpp
@@ -8,13 +8,12 @@ public:
             aux[i] = {position[i], speed[i]};
         }
         sort(aux.begin(),aux.end());
-        vector<float> time_to_target(n,0.0);
-        float last_max_time = FLT_MIN;
+        float last_max_time = FLT_MIN, time_to_target;
         int ans = 0;
         for (int i = 0;i<n;i++){
-            time_to_target[i] = float(target-aux[n-i-1][0])/float(aux[n-i-1][1]);
-            if (time_to_target[i]>last_max_time){
-                last_max_time = time_to_target[i];
+            time_to_target = float(target-aux[n-i-1][0])/float(aux[n-i-1][1]);
+            if (time_to_target>last_max_time){
+                last_max_time = time_to_target;
                 ans++;
             }
         }
