e0913f8d54c3c1a4a63b45b1a64bbf269f8bbf2a
Lucas Fraile <47170229+lucasfrailev@users.noreply.github.com>
2024-05-02 00:29:36 -0700

    Time: 135 ms (55.99%), Space: 74.8 MB (99.60%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.cpp
+++ 0853-car-fleet/0853-car-fleet.cpp
@@ -2,18 +2,17 @@ class Solution {
 public:
     int carFleet(int target, vector<int>& position, vector<int>& speed) {
         int n =position.size();
-        vector<pair<int,double>> aux;
-        for(int i = 0;i<n;i++){
-            aux.push_back({position[i],double(target-position[i])/double(speed[i])});
-        }
-        sort(aux.begin(), aux.end(), [](const pair<int, int>& a, const pair<int, int>& b) {
-            return a.first < b.first;
+        vector<int> aux(n,0);
+        iota(aux.begin(),aux.end(),0);
+        sort(aux.begin(), aux.end(), [&](int a, int b) {
+            return position[a] < position[b];
         });
         double last_max_time = DBL_MIN;
         int ans = 0;
         for (int i = n-1;i>=0;i--){
-            if (aux[i].second>last_max_time){
-                last_max_time = aux[i].second;
+            double time = (double(target-position[aux[i]])/double(speed[aux[i]]));
+            if (time>last_max_time){
+                last_max_time = time;
                 ans++;
             }
         }
