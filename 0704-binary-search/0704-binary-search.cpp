66bb414e11f7565804b5a217c54f5ef7dc547aa6
Lucas Fraile <47170229+lucasfrailev@users.noreply.github.com>
2024-05-05 16:21:43 -0700

    Time: 18 ms (96.08%), Space: 30 MB (21.71%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
         int l = 0, r = nums.size()-1, m = r/2;
         while(true){
             if (l>r){
