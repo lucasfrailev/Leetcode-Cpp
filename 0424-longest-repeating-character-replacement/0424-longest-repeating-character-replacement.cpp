89c9d5c8b3b8f2e3b8cf2532f3867951f82f87f0
Lucas Fraile <47170229+lucasfrailev@users.noreply.github.com>
2024-05-05 21:24:09 -0700

    Time: 7 ms (72.18%), Space: 8 MB (99.51%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        unordered_map<char,int> letter;
+        vector<int> letter(26, 0);
         int l = 0, maxf = 0, ans = 0;
         for (int i =0; i<s.size();i++){
-            letter[s[i]]+=1;
-            maxf = max(maxf,letter[s[i]]);
+            letter[s[i]-'A']+=1;
+            maxf = max(maxf,letter[s[i]-'A']);
             while((i-l+1-maxf)>k){
-                letter[s[l]]-=1;
+                letter[s[l]-'A']-=1;
                 l++;   
             }
             ans = max(ans,i-l+1);
