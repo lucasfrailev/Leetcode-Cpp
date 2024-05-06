f69fc02a716d53612667a3ad34ef216e94bfbaea
Lucas Fraile <47170229+lucasfrailev@users.noreply.github.com>
2024-05-05 18:37:16 -0700

    Time: 268 ms (64.33%), Space: 133.8 MB (46.57%) - LeetHub

--- 0981-time-based-key-value-store/0981-time-based-key-value-store.cpp
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.cpp
@@ -10,18 +10,20 @@ public:
     }
     
     string get(string key, int timestamp) {
-        int r = time_key[key].size()-1, l = 0;
+        const vector<pair<int, string>>& list =  time_key[key];
+        int r = list.size()-1, l = 0;
         if (r==-1){
             return "";
         } else{
+            if (list[0].first > timestamp) return "";
             string best = "";
             while(l<=r){
                 int m = l+(r-l)/2;
-                if (time_key[key][m].first==timestamp){
-                    return time_key[key][m].second;
+                if (list[m].first==timestamp){
+                    return list[m].second;
                 }
-                else if (time_key[key][m].first<timestamp){
-                    best= time_key[key][m].second;
+                else if (list[m].first<timestamp){
+                    best= list[m].second;
                     l = m+1;
                 } else {
                     r = m-1;
