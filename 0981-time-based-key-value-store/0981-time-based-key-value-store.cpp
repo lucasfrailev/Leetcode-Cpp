b6bf9613e339603bde6beeb8fba778000219098d
Lucas Fraile <47170229+lucasfrailev@users.noreply.github.com>
2024-05-05 18:20:58 -0700

    Time: 279 ms (47.03%), Space: 137 MB (6.15%) - LeetHub

--- 0981-time-based-key-value-store/0981-time-based-key-value-store.cpp
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.cpp
@@ -1,37 +1,23 @@
 class TimeMap {
 public:
-    unordered_map<string,pair<vector<int>, vector<string>>> time_key;
+    unordered_map<string,map<int,string>> time_key;
     TimeMap() {
     }
     
     void set(string key, string value, int timestamp) {
-        if (time_key[key].first.empty()){
-            time_key[key] = {{timestamp},{value}};
-        } else{
-            time_key[key].first.push_back(timestamp);
-            time_key[key].second.push_back(value);
-        }
+        time_key[key].insert({timestamp,value});
     }
     
     string get(string key, int timestamp) {
-        if (time_key[key].first.empty()){
+        if (time_key[key].empty()){
             return "";
-        } else{
-            int l = 0, r = time_key[key].first.size()-1, m = (r+l)/2;
-            string best = "";
-            while(l<=r){
-                if (time_key[key].first[m]==timestamp){
-                    return time_key[key].second[m];
-                }
-                else if (time_key[key].first[m]<timestamp){
-                    best= time_key[key].second[m];
-                    l = m+1;
-                } else {
-                    r = m-1;
-                }
-                m = (r+l)/2;
-            }
-            return best;
+        }
+        auto it = time_key[key].upper_bound(timestamp); 
+        if ( it == time_key[key].begin()){
+            return "";
+        } else {
+            --it;
+            return (*it).second;
         }
     }
 };
